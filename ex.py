0e1288056eb0653adb367e12bef34e5193e0520b
Mazlum Ağar <mazlumagar@users.noreply.github.com>
2014-11-30 00:10:56 +0200

    Update ex.py

--- ex.py
+++ ex.py
@@ -5,7 +5,8 @@ consumer_secret = "***"
 access_token = "***"
 access_token_secret = "***"
 
-remove = RemoveTweet(consumer_key, consumer_secret, access_token, access_token_secret)
-remove.remove_all_tweets("mazlumagar")
-
-
+try:
+    remove = RemoveTweet(consumer_key, consumer_secret, access_token, access_token_secret)
+    remove.remove_all_tweets("mazlumagar")
+except Exception, e:
+    print e
