ebf158562a4601c595b5eb8f5a6316ea52a42b36
Mazlum Ağar <mazlumagar@users.noreply.github.com>
2014-11-29 22:33:28 +0200

    Update ex.py

--- ex.py
+++ ex.py
@@ -1,10 +1,11 @@
-import remove_tweets
+from remove_tweets import RemoveTweet
 
 consumer_key = "***"
 consumer_secret = "***"
 access_token = "***"
 access_token_secret = "***"
 
-remove = remove_tweets.RemoveTweet(consumer_key, consumer_secret, access_token, access_token_secret)
+remove = RemoveTweet(consumer_key, consumer_secret, access_token, access_token_secret)
 remove.remove_all_tweets("mazlumagar")
+
 
